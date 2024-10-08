d423d17a865799d9a72fcc378c1d50ae9bef658a
Ajay Kumar <62602303+ajaythxkur@users.noreply.github.com>
2024-10-08 15:05:10 +0530

    $MEOW token added.

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,14 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [{
+    chainId: 1,
+    tokenAddress: "0x24dc0c6bd0a98f31961589e6432a8038833128f7c802ba148172553987c379f::MEOW::MEOW",
+    faAddress: null,
+    name: "Meowtos",
+    symbol: "MEOW",
+    decimals: 6,
+    logoUrl: "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/MEOW.svg",
+    websiteUrl: "https://meowtos.xyz",
+    coinGeckoId: null,
+    coinMarketCapId: null,
+}]
