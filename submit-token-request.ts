37b1756ad7bf0fed731f43bbc372992210cdaccf
Faptos <tabberfin22@gmail.com>
2024-10-14 23:09:07 +0800

    add FAPTOS token(meme)

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,18 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress:
+      "0x135726730c0e4e71652a6726354f148dec46974d5c152b0042ad58b37ea3c864::FaptOS::FaptOS",
+    faAddress: null,
+    name: "FaptOS",
+    symbol: "FaptOS",
+    decimals: 8,
+    logoUrl:
+      "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/FAPTOS.png",
+    websiteUrl: "https://faptosaptos.com",
+    coinGeckoId: null,
+    coinMarketCapId: null,
+  },
+]
