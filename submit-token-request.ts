ff5d796679f8c73e52c85a44c59bc42cd6032f3d
BabyEdog <hoangngockhanh.vn79@gmail.com>
2024-10-15 09:46:43 +0700

    Update submit-token-request.ts
    Add BEDOG

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,17 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+    "chainId": 1,
+    "tokenAddress": "0x2eebec013472480cc903cf7f118ccadcf813b4bf5e57aafba250746eff2150d5::BEDOG::BEDOG",
+    "faAddress": "0xa",
+    "name": "Baby Edog",
+    "symbol": "BEDOG",
+    "decimals": 6,
+    "logoUrl": "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/bedog.svg",
+    "websiteUrl": "https://edog.baby",
+    "coinGeckoId": "null",
+    "coinMarketCapId": null,
+  }
+]
+
