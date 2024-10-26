46e72d3ec1adc775c05989183ae8bbb42aed379b
Tiqan <tiqan.business@gmail.com>
2024-10-27 00:20:50 +0200

    add BURRITOS

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,19 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress:
+      "0xc89f2e0a7f61424e0b951936820f0423e3f076948dcdced112b76244ed840a25::burritos_coin::BurritosCoin",
+    faAddress:
+      "0xc89f2e0a7f61424e0b951936820f0423e3f076948dcdced112b76244ed840a25",
+    name: "BURRITOS",
+    symbol: "BURRITOS",
+    decimals: 9,
+    logoUrl:
+      "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/APT.svg",
+    websiteUrl: "https://aptosfoundation.org",
+    coinGeckoId: null,
+    coinMarketCapId: null,
+  },
+]
