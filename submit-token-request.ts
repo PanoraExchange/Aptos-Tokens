b2d87c72d2759a2e052bc84b41bcf3cca60681af
Greg Nazario <greg@gnazar.io>
2024-10-27 11:11:27 -0400

    [usdt] Submit request for USDT

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0x357b0b74bc833e95a115ad22604854d6b0fca151cecd94111770e5d6ffc9dc2b",
+    name: "Tether USD",
+    symbol: "USDt",
+    decimals: 6,
+    logoUrl: "https://tether.to/images/logoCircle.svg",
+    websiteUrl: "https://tether.to",
+    coinGeckoId: "tether",
+    coinMarketCapId: 825
+  }
+]
