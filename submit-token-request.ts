31e17a387b6ff82e8119867be7bec1814cda341d
Hayarobi Park <hayarobipark@gmail.com>
2024-11-07 16:05:16 +0900

    Registering GEM coin

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    "chainId": 1,
+    "tokenAddress": "0x9b49c6e930efb22a41d2e362efc605b266786fb847673d90b28d748f76b4c220::GEM::GEM",
+    "faAddress": null,
+    "name": "Global E-Money",
+    "symbol": "GEM",
+    "decimals": 8,
+    "logoUrl": "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/GEM.svg",
+    "websiteUrl": "https://gemzeitgeist.medium.com/",
+    "coinGeckoId": null,
+    "coinMarketCapId": null,
+  }
+]
