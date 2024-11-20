bf49deabb1d4879d36ff26b0fbd50a36019b1a8d
vah_13 <7976421+vah13@users.noreply.github.com>
2024-11-20 17:53:08 +0400

    Update submit-token-request.ts

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,14 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [{
+    chainId: 1,
+    tokenAddress: "0x55da7c7d7be00626cb5b993ac732c170bb23775890a69bb922e0e6cb5c7ae9bf::coin_factory::Emojicoin",
+    faAddress: null,
+    name: "PrideKiss",
+    symbol: "PRKD",
+    decimals: 6,
+    logoUrl: "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/PRKD.png",
+    websiteUrl: "",
+    coinGeckoId: null,
+    coinMarketCapId: null,
+}]
