452064b88a2412110628487cdf415f943ce0627b
Mark Ellis <104235389+mellis1010@users.noreply.github.com>
2024-11-26 12:48:04 -0700

    add MYCO token

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,15 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [{
+    "chainId": 1,
+    "tokenAddress": "0xcae49bc337709c74832f58096695315307b9ba694659dfaf7e6487b6a7561044::myco_token::MycoToken",
+    "faAddress": "0x98e39700e0bc5420d0a5c461a5420f1a17358041761f64147b173dfb9e21052d",
+    "name": "Myco",
+    "symbol": "MYCO",
+    "decimals": 8,
+    "logoUrl": "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/MYCO.svg",
+    "websiteUrl": "https://www.myco.io",
+    "coinGeckoId": null,
+    "coinMarketCapId": null
+  }]
+
