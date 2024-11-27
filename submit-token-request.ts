b759ca32199035ac526bbca7dac7ebeca488e8d1
Greg Nazario <greg@gnazar.io>
2024-11-26 23:45:29 -0500

    [request] Add night token

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0xef0d49f03e48dbd055c3a369f74a304c366bda148005ddf6bb881ced79da0b09",
+    name: "Night Coin",
+    symbol: "NIGHT",
+    decimals: 8,
+    logoUrl: "https://cdn.prod.website-files.com/640388e49053dc6d762dd327/673b864874a63fe32ddaa0f8_android-chrome-512x512.png",
+    websiteUrl: "https://midnight.io",
+    coinGeckoId: null,
+    coinMarketCapId: 34135
+  }
+]
