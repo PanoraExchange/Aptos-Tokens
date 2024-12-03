40fff6677c7e20636fc8e40f0729a6483bb71834
cylim <cylim226@gmail.com>
2024-12-03 19:09:48 +0800

    update ama token

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0xd0ab8c2f76cd640455db56ca758a9766a966c88f77920347aac1719edab1df5e",
+    name: "Amaterasu",
+    symbol: "AMA",
+    decimals: 8,
+    logoUrl: "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/AMA.png",
+    websiteUrl: "https://amaterasu.gg",
+    coinGeckoId: null,
+    coinMarketCapId: null
+  }
+]
