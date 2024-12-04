b6a9292ac5205d34baab90d8f98039a67b6ad08a
E <ipevgeny@gmail.com>
2024-12-04 13:26:20 +0400

    Add MOOMOO token info

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,19 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress:
+      "0xc5fbbcc4637aeebb4e732767abee8a21f2b0776f73b73e16ce13e7d31d6700da::MOOMOO::MOOMOO",
+    faAddress:
+      "0xc5fbbcc4637aeebb4e732767abee8a21f2b0776f73b73e16ce13e7d31d6700da",
+    name: "MOO MOO",
+    symbol: "MOOMOO",
+    decimals: 9,
+    logoUrl:
+      "https://github.com/eugene-x0/Aptos-Tokens/blob/f4526b9ad20dd6561c6ae36e309777fb29f60462/logos/MOOMOO.png",
+    websiteUrl: "https://www.moo.meme/",
+    coinGeckoId: "moo-moo",
+    coinMarketCapId: 34379,
+  },
+]
