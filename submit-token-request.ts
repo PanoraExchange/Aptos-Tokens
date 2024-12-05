6e8d7d8f2e6cd3f68148736f1cc03d834556c696
hanshaoxin <hanshaoxin@corp.netease.com>
2024-12-05 16:17:25 +0800

    update HTC

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        "chainId": 1,
+        "tokenAddress": "0x84501f9e18ce29a98b8c6fcddc0748b62387c5bab9832251b797d9cd0309b90e::htc::MoonCoin",
+        "faAddress": "0x84501f9e18ce29a98b8c6fcddc0748b62387c5bab9832251b797d9cd0309b90e",
+        "name": "HT Coin",
+        "symbol": "HTC",
+        "decimals": 6,
+        "logoUrl": "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/AMA.svg",
+        "websiteUrl": "https://aptosfoundation.org",
+        "coinGeckoId": null,
+        "coinMarketCapId": null,
+    }
+]
