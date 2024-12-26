745586b29332cc80848139bb438ede354c0b7920
WheatChain (wheat-sol) <wheat.sol.eco@gmail.com>
2024-12-26 17:58:25 +0100

    Update submit-token-request.ts

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,14 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0x216612b920bb14acd8ac7f80a845425895a068d2fcb422534aa8fc98ff794176",
+    name: "WheatChain",
+    symbol: "SWHIT",
+    decimals: 8,
+    logoUrl: "https://raw.githubusercontent.com/wheat-eco/Aptos-Tokens/refs/heads/main/logos/SWHIT.png",
+    websiteUrl: "http://wheatchain.xyz",
+  }
+]
