bf79782047b51c7599320223dc2883ef8a46d936
GEMzeitgeist <gem.zeitgeist@gmail.com>
2024-12-30 18:35:30 +0900

    Update submit-token-request.ts
    We are registering information about the GEM project.
    Please review it. :)

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,5 +1,4 @@
 import { SubmitTokenRequestInfo } from "types"
-  {
     "chainId": 1,
     "tokenAddress": "0x3fb7291a814eea979045f0bdb6b128a2a4f59533edad9d792069221586b92ab5::GEM::GEM",
     "faAddress": None
@@ -8,5 +7,4 @@ import { SubmitTokenRequestInfo } from "types"
     "decimals": 6,
     "logoUrl": "https://github.com/GEMzeitgeist/Aptos-Tokens/blob/e0c838f3938eea8cfbabdf2784e56c52cc044167/logos/GEM.svg websiteUrl: https://globalemoney.io/",
     "websiteUrl": "https://globalemoney.io/",
-      
 const requestList: SubmitTokenRequestInfo[] = []
