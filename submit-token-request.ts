7756649cc25b4e791ed18a680143ffa265324d7f
tuvuanh27 <vuanhtu111127@gmail.com>
2025-01-08 16:11:57 +0700

    add VIBE token

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,17 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress:
+      "0xeedba439a4ab8987a995cf5cfefebd713000b3365718a29dfbc36bc214445fb8",
+    name: "VibrantX token",
+    symbol: "VIBE",
+    decimals: 8,
+    logoUrl: "https://vibrantx.finance/vibe.svg",
+    websiteUrl: "https://vibrantx.finance",
+    coinGeckoId: null,
+    coinMarketCapId: null,
+  },
+]
