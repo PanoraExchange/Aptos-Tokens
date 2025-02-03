a4239831d1c5822bcdf34ef6b1a15dfa39b231bd
FandomFoundation <admin@cre8tor.io>
2025-02-03 14:54:24 +0900

    Add CRTR token

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+      "chainId": 1,
+      "tokenAddress": "0xe88123b4c983ec72a73e15a4292700f4bd368c776df8dce07e82388fd536d7fd::CRTR::CREATOR",
+      "faAddress": null,
+      "name": "CREATOR",
+      "symbol": "CRTR",
+      "decimals": 6,
+      "logoUrl": "https://cdn.fan-quest.com/static/crtr_coin.png",
+      "websiteUrl": "https://FAN-QUEST.com",
+      "coinGeckoId": null,
+      "coinMarketCapId": null,
+  }
+]
