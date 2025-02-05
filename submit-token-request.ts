a855120aaf786e12a074f0c0684e28164e962af3
Greg Nazario <greg@gnazar.io>
2025-02-05 15:55:00 -0500

    add usde

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,30 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress:
+      "0xf37a8864fe737eb8ec2c2931047047cbaed1beed3fb0e5b7c5526dafd3b9c2e9",
+    name: "USDe",
+    symbol: "USDe",
+    decimals: 6,
+    logoUrl: "https://arweave.net/qeSnRm_FIyp_khPfmg8o1zQeGO4AczDaEKe8jEUOzL4",
+    websiteUrl: "https://ethena.fi",
+    coinGeckoId: "ethena-usde",
+    coinMarketCapId: 29470
+  },
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress:
+      "0xb30a694a344edee467d9f82330bbe7c3b89f440a1ecd2da1f3bca266560fce69",
+    name: "sUSDe",
+    symbol: "sUSDe",
+    decimals: 6,
+    logoUrl: "https://arweave.net/iY7Aj5iwHKDIDL0m7QxeQN2URRrphWRpdKPJjE8fcaQ",
+    websiteUrl: "https://ethena.fi",
+    coinGeckoId: "ethena-staked-usde",
+    coinMarketCapId: 29471
+  }
+]
