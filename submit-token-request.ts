696f36ee3572d75ea2e71cb2af1c471bb549bb51
harispericorn <hari.k@spericorn.com>
2025-02-14 12:11:44 +0530

    sumbit token request WyndhamGarden1708

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        "chainId": 1,
+        "tokenAddress": null,
+        "faAddress": "0xe9c2f12eba13a46b27f42b4028bed6f702e7e12271dbebd08f310df52d064e81",
+        "name": "WyndhamGarden1708",
+        "symbol": "WG1708",
+        "decimals": 8,
+        "logoUrl": "https://ipfs.propbase.app/ipfs/QmYMqu6frfwYBo7Z2cFpyE5UfZWyKkHn6zMrVhCBW2iuQm",
+        "websiteUrl": "https://www.propbase.app/",
+        "coinGeckoId": null,
+        "coinMarketCapId": null,
+      }
+]
