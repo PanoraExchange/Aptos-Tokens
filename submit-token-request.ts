2e14a4f50bae98fe1ba2e3178fa033519e0bf486
Evan Witulski <evan@lucidfinance.xyz>
2025-02-18 11:07:23 -0800

    add PACT

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        "chainId": 1,
+        "tokenAddress": null,
+        "faAddress": "0xc546cc2dd26d9e9a4516b4514288bedf1085259fcb106b84b6469337f527fb92",
+        "name": "PACT",
+        "symbol": "PACT",
+        "decimals": 8,
+        "logoUrl": "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/PACT.png",
+        "websiteUrl": "https://pact.foundation",
+        "coinGeckoId": null,
+        "coinMarketCapId": null
+    }
+]
