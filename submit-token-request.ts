75fac15011aa5d8f1779ae47904195e8e815e228
wagmitt <wagmitt@gmail.com>
2025-03-05 02:00:38 -0500

    add submit request

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,28 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        chainId: 2,
+        tokenAddress: null,
+        faAddress: `0x82660bd244de65084f22a44a355293c1b0d907ed99be03a5586a680777030fb9`,
+        name: "Staked Kofi APT",
+        symbol: "stkAPT",
+        decimals: 8,
+        logoUrl: "https://alpha.kofi.finance/stkapt-logo.svg",
+        websiteUrl: "https://kofi.finance",
+        coinGeckoId: null,
+        coinMarketCapId: null,
+      },
+      {
+        chainId: 2,
+        tokenAddress:  null,
+        faAddress: `0xb6c131f76bc27342b2f2d15aa0f77a1435a5c55aac6d196875c17ed85affb74f`,
+        name: 'Kofi APT',
+        symbol: 'kAPT',
+        decimals: 8,
+        logoUrl: "https://alpha.kofi.finance/kapt-logo.svg",
+        websiteUrl: "https://kofi.finance",
+        coinGeckoId: null,
+        coinMarketCapId: null,
+      }
+]
