351e2b48f3f8bb731d64d8b4f064098038bef20a
yuyin-zhang <106600353+yuyin-zhang@users.noreply.github.com>
2025-03-12 18:01:15 +1100

    add AGLP token

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        chainId: 1,
+        tokenAddress: "0x570b5bd56a15b626745cede6cafc64a0ef1d86465d033e4e3da1b2a07e75f746",
+        faAddress: "0x570b5bd56a15b626745cede6cafc64a0ef1d86465d033e4e3da1b2a07e75f746",
+        name: "AGLP",
+        symbol: "AGLP",
+        decimals: 6,
+        logoUrl: "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/AGLP.svg",
+        websiteUrl: "https://agdex.io",
+        coinGeckoId: "",
+        coinMarketCapId: null
+    },
+]
