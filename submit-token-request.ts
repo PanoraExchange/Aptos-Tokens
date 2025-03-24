ccb229b99fadccdf1a3f6c8f87924ee6d2889493
John Amnis <143375741+john2798@users.noreply.github.com>
2025-03-24 23:17:50 +0700

    Add AMI token

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    "chainId": 1,
+    "tokenAddress": null,
+    "faAddress": "0xb36527754eb54d7ff55daf13bcb54b42b88ec484bd6f0e3b2e0d1db169de6451",
+    "name": "AMNIS",
+    "symbol": "AMI",
+    "decimals": 8,
+    "logoUrl": "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/AMI.png",
+    "websiteUrl": "https://amnis.finance",
+    "coinGeckoId": null,
+    "coinMarketCapId": null,
+  }
+]
