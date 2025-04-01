e9d148efe663d950041c3cc4de78cd0d3e062e52
Whoiskp <khoapham.ptithcm@gmail.com>
2025-04-01 11:38:55 +0700

    add TEVI Coin

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        "chainId": 1,
+        "tokenAddress": null,
+        "faAddress": "0x92f0951cecb35dc7613bc0e09f27e2c7aaebdc320f93901c121a1ec06f3aa455",
+        "name": "TEVI Coin",
+        "symbol": "TEVI",
+        "decimals": 8,
+        "logoUrl": "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/TEVI.svg",
+        "websiteUrl": "https://tevi.com",
+        "coinGeckoId": null,
+        "coinMarketCapId": null
+    }
+]
