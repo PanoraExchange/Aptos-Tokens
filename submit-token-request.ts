f13c4a97b09c6e38bcb31396533ee49f4fa12978
wagmitt <wagmitt@gmail.com>
2025-05-12 19:03:45 +1000

    add kAPT and stkAPT

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,28 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        chainId: 1,
+        tokenAddress: "0x42556039b88593e768c97ab1a3ab0c6a17230825769304482dff8fdebe4c002b",
+        faAddress: "0x42556039b88593e768c97ab1a3ab0c6a17230825769304482dff8fdebe4c002b",
+        name: "Kofi Staked APT",
+        symbol: "stkAPT",
+        decimals: 8,
+        logoUrl: "stkAPT.png",
+        websiteUrl: "https://app.kofi.finance",
+        coinGeckoId: null,
+        coinMarketCapId: null
+    },
+    {
+        chainId: 1,
+        tokenAddress: "0x821c94e69bc7ca058c913b7b5e6b0a5c9fd1523d58723a966fb8c1f5ea888105",
+        faAddress: "0x821c94e69bc7ca058c913b7b5e6b0a5c9fd1523d58723a966fb8c1f5ea888105",
+        name: "Kofi APT",
+        symbol: "kAPT",
+        decimals: 8,
+        logoUrl: "kAPT.png",
+        websiteUrl: "https://app.kofi.finance",
+        coinGeckoId: null,
+        coinMarketCapId: null
+    }
+]
