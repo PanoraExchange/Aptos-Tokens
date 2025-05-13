3009515f5cf7dcb5d33ad79ac9fc20ae40eaed54
jameswgc <154668930+jameswgc@users.noreply.github.com>
2025-05-13 09:37:15 +0700

    token listing request for WGC

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0xaef817092359dfa234e87490611c42aa1597371df367286b641ea5d9f7a69de9",
+    name: "Wild Goat Coin",
+    symbol: "WGC",
+    decimals: 6,
+    logoUrl: "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/WGC.svg",
+    websiteUrl: "https://wildgoatcoin.com",
+    coinGeckoId: "wild-goat-coin-2",
+    coinMarketCapId: null,
+  }
+]
