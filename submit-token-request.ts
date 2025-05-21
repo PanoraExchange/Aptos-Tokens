dbd27882d95c7e7580955691220bd7aaf2dab1cf
kenvin-apt <kenvin@meso.finance>
2025-05-21 10:34:24 +0700

    listing MESO token

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0x378d5ba871c3d1bdf477a617f997f23d9e0702de97a02f42925b44fa3abc9866",
+    name: "MESO",
+    symbol: "MESO",
+    decimals: 8,
+    logoUrl: "https://raw.githubusercontent.com/PanoraExchange/Aptos-Tokens/main/logos/meso.png",
+    websiteUrl: "https://meso.finance/",
+    coinGeckoId: null,
+    coinMarketCapId: null,
+  }
+]
