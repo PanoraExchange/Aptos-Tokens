9e520066ae972b98fcb183177a75a0b337e2add9
Saul Dope <saul@mirage.money>
2025-05-25 23:42:25 -0400

    add mirage stablecoin mUSD to token list

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        chainId: 1,
+        tokenAddress: null,
+        faAddress: "0xdd84125d1ebac8f1ecb2819801417fc392325e672be111ec03830c34d6ff82dd",
+        name: "mUSD",
+        symbol: "mUSD",
+        decimals: 8,
+        logoUrl: "https://api.mirage.money/assets/mUSD.svg",
+        websiteUrl: "https://mirage.money",
+        coinGeckoId: null,
+        coinMarketCapId: null,
+      }
+]
