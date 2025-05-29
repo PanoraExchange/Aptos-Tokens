16faa5da90d0221e0b155c5398e7f8a3301d8433
javed828 <56409965+javed828@users.noreply.github.com>
2025-05-29 13:01:02 +0530

    Update submit-token-request.ts

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    "chainId": 1,
+    "tokenAddress": null,
+    "faAddress": "0x44a90ab72bfd1b2637e68cba808f018217bec27b1a83c265c9eb298458ce0172",
+    "name": "XUHT",
+    "symbol": "XUHT",
+    "decimals": 8,
+    "logoUrl": "https://raw.githubusercontent.com/javed828/Aptos-Tokens/refs/heads/main/logos/XUHT.svg",
+    "websiteUrl": "https://uht.xyz/",
+    "coinGeckoId": null,
+    "coinMarketCapId": null,
+  }
+]
