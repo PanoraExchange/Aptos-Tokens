61897a96b3c09e5401eccdfd96e895844293ceef
tatalock <tatalock1010@gmail.com>
2025-07-09 23:46:49 +0800

    add RION to token list

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,18 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress:
+      "0x435ad41e7b383cef98899c4e5a22c8dc88ab67b22f95e5663d6c6649298c3a9d",
+    name: "Hyperion",
+    symbol: "RION",
+    decimals: 6,
+    logoUrl:
+      "https://khnk24lv2ydj52ib5vomdmtlxhxxmvvyztzaa2jnrsgzk4ff4yfq.arweave.net/UdqtcXXWBp7pAe1cwbJrue92VrjM8gBpLYyNlXCl5gs",
+    websiteUrl: "https://hyperion.xyz/",
+    coinGeckoId: null,
+    coinMarketCapId: null,
+  },
+]
