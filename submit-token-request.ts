70cb99e048240b825c4b861428e53d242239b8b4
fiamma-builder <panos@fiammachain.io>
2025-08-04 23:28:36 +0800

    feat: add fiamma btc

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        "chainId": 1,
+        "tokenAddress": null,
+        "faAddress": "0x75de592a7e62e6224d13763c392190fda8635ebb79c798a5e9dd0840102f3f93",
+        "name": "Fiamma BTC",
+        "symbol": "FIABTC",
+        "decimals": 8,
+        "logoUrl": "https://s3.fiammalabs.io/FiaBTC.svg",
+        "websiteUrl": "https://fiammalabs.io",
+        "coinGeckoId": null,
+        "coinMarketCapId": null
+    }
+]
