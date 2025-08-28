e0389bf85e198b5f858ea69db9433f6db97f4711
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2025-08-28 14:30:37 +0200

    add: LINK

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0x8c764993820ea735719f1ff7f1a0f80c022b18e7b5daefa35adf60a3a6556566",
+    name: "ChainLink Token",
+    symbol: "LINK",
+    decimals: 8,
+    logoUrl: "https://raw.githubusercontent.com/smartcontractkit/documentation/main/public/assets/icons/link.svg",
+    websiteUrl: "https://chain.link",
+    coinGeckoId: "chainlink",
+    coinMarketCapId: 1975
+  }
+]
