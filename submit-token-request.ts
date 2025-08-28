38898fd79c84fb5576ab602ef69eb05d7d5441a4
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2025-08-28 14:47:56 +0200

    add: brBTC and uniBTC

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,27 @@
-import { SubmitTokenRequestInfo } from "types"
+import {SubmitTokenRequestInfo} from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0x8e51106b139001f1f25a320066621a2e0d140724ee9be1d49aaf9e76ceb24d75",
+    name: "Bedrock BTC",
+    symbol: "brBTC",
+    decimals: 8,
+    logoUrl: "https://raw.githubusercontent.com/Bedrock-Technology/bedrock-static/main/logo/brBTC.svg",
+    websiteUrl: "https://www.bedrock.technology",
+    coinGeckoId: "bedrock-btc",
+    coinMarketCapId: null
+  }, {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress: "0xf764dbfd6999067ac052a8e722ae359bec389bd7dba19ead586801b99b81b075",
+    name: "Universal BTC",
+    symbol: "uniBTC",
+    decimals: 8,
+    logoUrl: "https://raw.githubusercontent.com/Bedrock-Technology/bedrock-static/main/logo/unibtc.svg",
+    websiteUrl: "https://www.bedrock.technology",
+    coinGeckoId: "universal-btc",
+    coinMarketCapId: 36175
+  }
+]
