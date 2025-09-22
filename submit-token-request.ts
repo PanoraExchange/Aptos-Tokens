e0646131d412d7f5d5d29cd941e23e89b530c339
Greg Nazario <greg@gnazar.io>
2025-09-22 15:38:11 -0400

    add usd1

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,17 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+  {
+    chainId: 1,
+    tokenAddress: null,
+    faAddress:
+      "0x05fabd1b12e39967a3c24e91b7b8f67719a6dacee74f3c8b9fb7d93e855437d2",
+    name: "World Liberty Financial USD",
+    symbol: "USD1",
+    decimals: 6,
+    logoUrl: "https://raw.githubusercontent.com/worldliberty/usd1-metadata/refs/heads/main/logo.png",
+    websiteUrl: "https://worldlibertyfinancial.com/",
+    coinGeckoId: "usd1-wlfi",
+    coinMarketCapId: 36148
+  }
+]
