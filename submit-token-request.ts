4cc70036abdc08738750a21221a4a4894da63b3b
alexatpact <alex@pactfoundation.com>
2025-12-30 10:56:11 +0100

    Update submit-token-request.ts
    PACT token information - TO PROVIDE MORE (e.g. Coinmarketcap, Coingecko).
    
    Blacklist for now, please.

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,15 @@
 import { SubmitTokenRequestInfo } from "types"
 
 const requestList: SubmitTokenRequestInfo[] = []
+  {
+    "chainId": 1,
+    "tokenAddress": "0xc546cc2dd26d9e9a4516b4514288bedf1085259fcb106b84b6469337f527fb92",
+    "faAddress": "https://explorer.aptoslabs.com/fungible_asset/0xc546cc2dd26d9e9a4516b4514288bedf1085259fcb106b84b6469337f527fb92?network=mainnet",
+    "name": "PACT Token",
+    "symbol": "PACT",
+    "decimals": 8,
+    "logoUrl": "https://drive.google.com/file/d/1ILmUbcoFCv899irlvYQt_28_2VV5Nxr6/view?usp=sharing",
+    "websiteUrl": "https://pactfoundation.com/",
+    "coinGeckoId": "null",
+    "coinMarketCapId": "null",
+  }
