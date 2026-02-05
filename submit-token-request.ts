27ebe68b118d3b571e2ee4f070649f5b84978f69
Md. Ariful Islam <86847969+ArifulIslam99@users.noreply.github.com>
2026-02-05 17:31:25 +0600

    Add token details for Panora List

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,16 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+        "chainId": 1,
+        "tokenAddress": null,
+        "faAddress": "0x65657e7a844dde1602b87473e89e5e2e89ecbf674fedfb22e909bf27a02f8fb1",
+        "name": "FAME",
+        "symbol": "FAME",
+        "decimals": 8,
+        "logoUrl": "https://raw.githubusercontent.com/hypercutprotocol/Aptos-Tokens/main/logos/FAME.svg",
+        "websiteUrl": "https://www.hypercut.tv/",
+        "coinGeckoId": null,
+        "coinMarketCapId": null,
+      }
+]
