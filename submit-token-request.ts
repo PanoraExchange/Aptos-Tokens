059e76404001ab42f285566c3bf01f2e9c115f86
Cerwyn <96032711+Cerwyn-UCT@users.noreply.github.com>
2026-02-11 13:17:21 +0700

    add TAPP to token list

--- submit-token-request.ts
+++ submit-token-request.ts
@@ -1,3 +1,17 @@
 import { SubmitTokenRequestInfo } from "types"
 
-const requestList: SubmitTokenRequestInfo[] = []
+const requestList: SubmitTokenRequestInfo[] = [
+    {
+      chainId: 1,
+      tokenAddress: null,
+      faAddress:
+        "0x7e464f3d2c1aed51cdcbdc653cc5c21526fe424bed2944bdd21dabb389dd7921",
+      name: "Tapp Exchange",
+      symbol: "TAPP",
+      decimals: 8,
+      logoUrl: "https://cdn.prod.tapp-dex.devucc.name/assets/TAPP.svg",
+      websiteUrl: "https://tapp.exchange/",
+      coinGeckoId: null,
+      coinMarketCapId: null,
+    },
+  ]
